792c23defb7ea125db66e0652b84129b71c1858c
David <davidverweij@gmail.com>
2021-10-22 17:31:27 +0100

    example test for CLI

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import click.testing
 import pytest
 
@@ -9,7 +11,18 @@ def runner():
     return click.testing.CliRunner()
 
 
-def test_main_succeeds(runner):
+def test_version_succeeds(runner):
+
     result = runner.invoke(cli.version)
 
+    assert result.exit_code == 0
+
+
+@patch("ideafast_etl.utils.cli.run_command")
+def test_abc_aborted(mock_run_command, runner):
+    bump_type = "patch"
+
+    result = runner.invoke(cli.bump, ["-b", bump_type])
+
+    mock_run_command.assert_any_call(f"poetry version {bump_type}", True)
     assert result.exit_code == 0

80600f1a238ae3c1544c543d466fa5c92b6d1f48
David <davidverweij@gmail.com>
2021-10-22 16:58:16 +0100

    Revert "fix get latest docker tag"
    This reverts commit 3ffc805ba6b55046e1cde35f62c95d5192256879.

--- ideafast_etl/utils/cli.py
+++ ideafast_etl/utils/cli.py
@@ -22,8 +22,7 @@ def get_version() -> Optional[str]:
 
 def get_latest_docker_tag() -> Optional[str]:
     res = run_command(
-        f"docker images {DOCKER_REGISTRY} --format {{{{.Tag}}}} "
-        f"| sort -r | grep -v 'latest' | head -n 1",
+        f"docker images {DOCKER_REGISTRY} --format {{{{.Tag}}}} | sort | head -n 1",
         True,
     )
     return res.stdout.decode("ascii").rstrip() or None

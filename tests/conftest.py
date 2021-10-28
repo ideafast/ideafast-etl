724f096f25e0040e277ac33445a4f7da9ab16fc5
David <davidverweij@gmail.com>
2021-10-28 13:53:45 +0100

    move fixtures to conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 import importlib
 import sys
+from pathlib import Path
 from typing import Any, Generator
 
 import pytest
+from airflow.models import DagBag
+
+PROJECT_DIR = Path(__file__).parent.parent
 
 
 class localise_utils:
@@ -13,8 +17,18 @@ class localise_utils:
         del sys.modules["etl_utils"]
 
 
-@pytest.fixture
-def utils_module() -> Generator[None, None, None]:
+@pytest.fixture()
+def local_utils_module() -> Generator[None, None, None]:
     """Use the Airflow DAG localised module within the test suite (argubaly a hack)"""
     with localise_utils():
         yield
+
+
+@pytest.fixture()
+def dagbag(local_utils_module: Generator[None, None, None]) -> DagBag:
+    """Load the dags from the Airflow folder, with local imports overridden in conftest.py"""
+    return DagBag(
+        # Dag folder needs to be identical as used in docker-compose.yaml
+        dag_folder=Path(PROJECT_DIR) / "ideafast_etl/dags",
+        include_examples=False,
+    )
--- tests/test_example_mongo_dag.py
+++ tests/test_example_mongo_dag.py
@@ -1,20 +1,4 @@
-from pathlib import Path
-from typing import ContextManager
-
-import pytest
 from airflow.models import DagBag
-
-project_dir = Path(__file__).parent.parent
-
-
-@pytest.fixture()
-def dagbag(utils_module: ContextManager) -> DagBag:
-    """Load the dags from the Airflow folder, with local imports overridden in conftest.py"""
-    return DagBag(
-        # Dag folder needs to be identical as used in docker-compose.yaml
-        dag_folder=Path(project_dir) / "ideafast_etl/dags",
-        include_examples=False,
-    )
 
 
 def test_example_mongo_dag_loaded(dagbag: DagBag) -> None:

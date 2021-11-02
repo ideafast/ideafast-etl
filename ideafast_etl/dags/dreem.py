632da1f4acc691d2c6efc5ccca358e747ea51d20
David <davidverweij@gmail.com>
2021-11-02 15:20:22 +0000

    Revert "create Dreem specific record type"
    This reverts commit 5bc0145c04cfd228dd38e40548c6397a97725ebd.

--- ideafast_etl/dags/dreem.py
+++ ideafast_etl/dags/dreem.py
@@ -6,7 +6,7 @@ from typing import Optional
 from airflow import DAG
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.python import PythonOperator
-from etl_utils.db import DeviceType, DreemRecord, create_many_records, get_hashes
+from etl_utils.db import DeviceType, Record, create_many_records, get_hashes
 from etl_utils.hooks.drm import DreemJwtHook
 
 # DAG setup with tasks
@@ -38,19 +38,16 @@ with DAG(
 
             # generator for new unknown records
             new_record_gen = (
-                DreemRecord(
+                Record(
                     _id=None,
                     hash=hash,
                     manufacturer_ref=r["id"],
-                    device_serial=r["device"],
-                    download_url=r["url"],
                     device_type=DeviceType.DRM,
                     start=datetime.fromtimestamp(r["report"]["start_time"]),
                     end=datetime.fromtimestamp(r["report"]["stop_time"]),
                 )
                 for r in result
-                if (hash := DreemRecord.generate_hash(r["id"], DeviceType.DRM))
-                not in known
+                if (hash := Record.generate_hash(r["id"], DeviceType.DRM)) not in known
             )
 
             new_records = list(islice(new_record_gen, limit))
@@ -62,29 +59,13 @@ with DAG(
                 f"{len(new_ids)} new Dreem records added to the DB (limit was {limit})"
             )
 
-    def _resolve_device_ids(limit: Optional[int] = None) -> None:
-        """
-        Retrieve database records with unresolved device IDs, and try to resolve them
-
-        Parameters
-        ----------
-        limit : None | int
-            limit of how many records to handle this run - useful for testing
-            or managing workload in batches
-        """
-        pass
-
     # Set all tasks
     download_latest_dreem_metadata = PythonOperator(
         task_id="download_latest_dreem_metadata",
         python_callable=_download_latest_dreem_metadata,
         op_kwargs={"limit": 1},
     )
-    resolve_device_ids = PythonOperator(
-        task_id="resolve_device_ids",
-        python_callable=_resolve_device_ids,
-        op_kwargs={"limit": 1},
-    )
+    resolve_device_ids = DummyOperator(task_id="resolve_device_ids")
     resolve_patient_ids = DummyOperator(task_id="resolve_patient_ids")
     extract_prep_load = DummyOperator(
         task_id="extract_prep_load",

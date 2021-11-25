9e9743b7ef7eb70b0dd210cf1b2ea82008a901d4
David <davidverweij@gmail.com>
2021-11-25 12:29:40 +0000

    exclude tests from typing and flake8 formatting

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -2,8 +2,7 @@ from ideafast_etl.hooks.db import DeviceType
 from ideafast_etl.operators.ucam import ResolveDeviceIdOperator
 
 
-def test_ucam_operator() -> None:
-    """Test operator"""
+def test_ucam_operator():
     result = ResolveDeviceIdOperator(task_id="test", device_type=DeviceType.BTF)
 
     assert result == result

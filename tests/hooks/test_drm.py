f41a06814b95cb8485c7ad54a27a30e72a45ae82
David <davidverweij@gmail.com>
2021-12-13 17:15:18 +0100

    minor update on drm tests

--- tests/hooks/test_drm.py
+++ tests/hooks/test_drm.py
@@ -41,18 +41,15 @@ def test_metadata_call_empty():
 
 def test_download_file_success(mock_requests_drm, tmp_path):
     """Test that a file is downloaded, and in the correct folder"""
-
+    chunk_size = 1024
+    total_chunks = 3
     mock_conn = Mock()
     mock_conn.get.return_value.json.return_value = {"data_url": "test_url"}
     mock_requests_drm.get.return_value.__enter__.return_value.headers.get.return_value = (
-        2048
+        chunk_size * total_chunks
     )
     mock_requests_drm.get.return_value.__enter__.return_value.iter_content.return_value = iter(
-        [
-            "chunk1".encode("utf-8"),
-            "_".encode("utf-8"),
-            "chunk2".encode("utf-8"),
-        ]
+        [bytearray(chunk_size) for _ in range(total_chunks)]
     )
 
     with patch.object(DreemHook, "get_conn", return_value=mock_conn):
@@ -61,10 +58,8 @@ def test_download_file_success(mock_requests_drm, tmp_path):
 
         result = drm_hook.download_file("test_ref", tmp_path)
 
-        with open(tmp_path / "test_ref.h5") as f:
-            stored = f.read()
-            assert stored == "chunk1_chunk2"
-            assert len(list(tmp_path.iterdir())) == 1
+        assert len(list(tmp_path.iterdir())) == 1
+        assert (tmp_path / "test_ref.h5").stat().st_size == chunk_size * total_chunks
 
 
 def test_download_file_no_data_url(tmp_path):
